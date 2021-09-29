7ce3ad6db8638195a19f0b3c99277b60ca59e1d9
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-09-29 09:25:40 -0600

    Create file for DOM updates and move in relevant functions

---
+++ src/domUpdates.js
@@ -0,0 +1,22 @@
+const renderBookings = (guestBookings) => {
+  guestBookings.map(booking => {
+    document.querySelector('.guest-bookings').insertAdjacentHTML(
+      'beforeEnd', `<p>You booked room ${booking.roomNumber} on ${booking.date}</p>`
+    );
+  });
+}
+
+const renderDashboard = (guestInfo) => {
+  document.querySelector('.login-div').classList.add('hidden');
+  document.querySelector('.bookings-container').classList.remove('hidden');
+  document.querySelector('.room-options-container').classList.remove('hidden');
+  document.querySelectorAll('.search-container').forEach(container => container.classList.remove('hidden'));
+  document.querySelector('main').insertAdjacentHTML('beforeBegin', `
+  <h3 class="welcome-message">Welcome back, ${guestInfo.name}</h3>
+  `);
+}
+
+export {
+  renderBookings,
+  renderDashboard,
+}
