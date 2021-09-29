55e5a791b22e369c810332e78aef6fb5ddb91e3c
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-09-29 10:34:45 -0600

    Update test data in dashboard

--- test/Dashboard-test.js
+++ test/Dashboard-test.js
@@ -3,56 +3,69 @@ import Dashboard from '../src/classes/Dashboard';
 
 describe('Dashboard class', () => {
   let dashboard;
-  beforeEach(function() {
+  beforeEach(function () {
     dashboard = new Dashboard([{
-        roomType: 'single room', 
-        roomNumber: 1
-      }, {
-        roomType: 'suite', 
-        roomNumber: 2
-      }], 
+      roomType: 'single room',
+      roomNumber: 1
+    }, {
+      roomType: 'suite',
+      roomNumber: 2
+    }],
       [{
-        roomType: 'residential suite', 
-        roomNumber: 7
+        date: "2020/04/22",
+        id: "5fwrgu4i7k55hl6sz",
+        roomNumber: 15,
+        roomServiceCharges: [],
+        userID: 9
       }, {
-        roomType: 'single room', 
-        roomNumber: 10
+        date: "2020/01/24",
+        id: "5fwrgu4i7k55hl6t5",
+        roomNumber: 24,
+        roomServiceCharges: [],
+        userID: 43
       }
-    ])
+      ])
   });
-  
+
   it('should be an instance of Dashboard', () => {
     expect(dashboard).to.be.an.instanceof(Dashboard);
   })
 
   it('should start with a date property', () => {
     const expected = JSON.stringify(new Date())
-    .split('T')[0]
-    .split('"')[1]
-    .split('-')
-    .join('/');
+      .split('T')[0]
+      .split('"')[1]
+      .split('-')
+      .join('/');
     expect(dashboard.currentDate).to.equal(expected);
   })
 
 
 
   it('should start with bookings', () => {
-    const expected = [{
-      roomType: 'residential suite', 
-      roomNumber: 7
+    const expected = [[{
+      date: "2020/04/22",
+      id: "5fwrgu4i7k55hl6sz",
+      roomNumber: 15,
+      roomServiceCharges: [],
+      userID: 9
     }, {
-      roomType: 'single room', 
-      roomNumber: 10
-    }]
+      date: "2020/01/24",
+      id: "5fwrgu4i7k55hl6t5",
+      roomNumber: 24,
+      roomServiceCharges: [],
+      userID: 43
+    }
+    ]]
     expect(dashboard.bookings).to.deep.equal(expected);
   })
 
   it('should start with rooms', () => {
     const expected = [{
-      roomType: 'single room', 
+      roomType: 'single room',
       roomNumber: 1
     }, {
-      roomType: 'suite', 
+      roomType: 'suite',
       roomNumber: 2
     }]
     expect(dashboard.rooms).to.deep.equal(expected);
@@ -60,10 +73,10 @@ describe('Dashboard class', () => {
 
   it('should update currentRooms with All', () => {
     const expected = [{
-      roomType: 'single room', 
+      roomType: 'single room',
       roomNumber: 1
     }, {
-      roomType: 'suite', 
+      roomType: 'suite',
       roomNumber: 2
     }]
     dashboard.filterRoomByType('All');
@@ -72,7 +85,7 @@ describe('Dashboard class', () => {
 
   it('should update currentRooms with different roomTypes', () => {
     const expected = [{
-      roomType: 'single room', 
+      roomType: 'single room',
       roomNumber: 1
     }]
     dashboard.filterRoomByType('single-room');
@@ -80,8 +93,8 @@ describe('Dashboard class', () => {
   })
 
   it('should update the currentDate when updateCurrentRooms is called', () => {
-    dashboard.updateCurrentRooms(2020/11/1);
-    expect(dashboard.currentDate).to.equal(2020/11/1);
+    dashboard.updateCurrentRooms(2020 / 11 / 1);
+    expect(dashboard.currentDate).to.equal(2020 / 11 / 1);
   })
 
 })
