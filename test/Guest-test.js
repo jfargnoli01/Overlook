59c459768e6094aea638e7ebfff94f8d79a78e53
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-09-29 09:20:46 -0600

    Update Guest and Dashboard tests

--- test/Guest-test.js
+++ test/Guest-test.js
@@ -2,19 +2,38 @@ import { expect } from 'chai';
 import Guest from '../src/classes/Guest';
 
 describe('Guest class', () => {
+  let guest;
+  
+  beforeEach(function () {
+    guest = new Guest({ id: 1, name: 'Juni' }, [{
+      roomType: 'junior suite',
+      roomNumber: 3
+    }, {
+      roomType: 'suite',
+      roomNumber: 4
+    }]);
+  })
 
   it('should be an instance of Guest', () => {
-    const guest = new Guest(1, 'Juni');
     expect(guest).to.be.an.instanceof(Guest);
   })
 
   it('should start with an id', () => {
-    const guest = new Guest(1, 'Juni');
     expect(guest.id).to.equal(1);
   })
 
   it('should start with a name', () => {
-    const guest = new Guest(1, 'Juni');
     expect(guest.name).to.equal('Juni');
   })
-} )
+
+  it('should start with guestBookings', () => {
+    const expected = [{
+      roomType: 'junior suite',
+      roomNumber: 3
+    }, {
+      roomType: 'suite',
+      roomNumber: 4
+    }]
+    expect(guest.bookings).to.deep.equal(expected);
+  })
+})
